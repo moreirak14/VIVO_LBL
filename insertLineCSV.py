27c288fbd2d1c175fe8a81d0b6c41afbb29fe9dd
Moreira Kaique <moreirak@gi-de.com>
2021-08-04 10:09:43 -0300

    Estruturando main.py para o processamento completo

--- insertLineCSV.py
+++ insertLineCSV.py
@@ -1,20 +1,24 @@
 import csv
 from pathlib import Path
-
+from turtle import pd
 
 BASE_DIR = Path("CSV/")
 csv_path = BASE_DIR / "lbl_scgn000026_20210716164414.csv"
-json_path = 'JSON/pythonJSON.json'
-
 
 def lineCSV():
 
-    input_names_str = "INPUT;REGIONAL;CEP;BAIRRO;MUNICIPIO;ENDERECO"
-    input_names = [input_names_str]
+    input_names_str = ['INPUT;''REGIONAL;''CEP;''BAIRRO;''MUNICIPIO;''ENDERECO']
 
-    with open(csv_path, 'r+', newline='') as file_csv:
-        writer = csv.DictWriter(file_csv, fieldnames=input_names)
+    with open(csv_path, 'w') as file_csv:
+        writer = csv.DictWriter(file_csv, fieldnames=input_names_str)
         writer.writeheader()
 
+def lineCSV2():
+
+    input_names_str = ['INPUT;''REGIONAL;''CEP;''BAIRRO;''MUNICIPIO;''ENDERECO']
+
+    df = pd.read_csv(csv_path, header=None)
+    df.to_csv("example.csv", header=["Letter", "Number", "Symbol"], index=False)
+
 if __name__ == "__main__":
-    lineCSV()
+    lineCSV2()
--- main.py
+++ main.py
@@ -7,28 +7,40 @@ from pathlib import Path
 JSON_FILE_PATH = "JSON/pythonJSON.json"
 path_lbl = "LBL/"
 filename_filter = "VIVO*.LBL"
+path_filename_filter = "LBL/VIVO0212248.LBL"
 
 
 def list_inputs():
     list = os.listdir(path_lbl)
     number_files = len(list)
-    #print(number_files)
 
     # Verifica se existe algum arquivo .lbl para ser ajustado
     if (number_files == 0):
         tkinter.messagebox.showwarning(title="AVISO", message="Não têm nenhum arquivo LBL para ser ajustado")
 
+    elif (number_files != 0):
+        return readerJSON()
+
 
 def readerJSON():
-    # Lista todos inputs de determinado diretorio
+    # Lista todos inputs do diretorio LBL/
     for filename in Path(path_lbl).rglob(filename_filter):
-        print(filename)
+        inputsfile = [filename.name]
+        print(inputsfile)
 
     with open(JSON_FILE_PATH, "r") as json_file:
         json_data = json.load(json_file)
         print(json_data)
 
+    # Pega os dados do INPUT referente ao arquivo LBL
+    input_data = None
+    for input_ in json_data["INPUTs"]:
+        if input_["INPUT"] == inputsfile:
+            input_data = input_
+
+    with open(path_filename_filter, "r") as lbl_file:
+        lbl_file_lines = lbl_file.readlines()
+
 
 if __name__ == '__main__':
     list_inputs()
-    readerJSON()
